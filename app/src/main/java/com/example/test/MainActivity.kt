cc19949aa4b84784646f326994ffcb7f2d316452
sm1374 <sm1374@naver.com>
2020-08-31 14:32:05 +0900

    아래에서 위로올라오면서 커지는 애니메이션

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -1,31 +1,43 @@
 package com.example.test
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Point
+import android.os.Build
 import android.os.Bundle
+import android.util.DisplayMetrics
+import android.util.Log
 import android.view.View
-import android.view.animation.Animation
-import android.view.animation.AnimationUtils
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
+
 class MainActivity : AppCompatActivity() {
+    var dp = 0f
+
+    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var anim : Animation = AnimationUtils.loadAnimation(this, R.anim.anim_translate1)
-        imageView.startAnimation(anim)
-        anim.setAnimationListener(testListener)
 
+        val displayMetrics = DisplayMetrics()
+        this@MainActivity.windowManager.defaultDisplay.getMetrics(displayMetrics)
+
+        val deviceHeight = displayMetrics.heightPixels
+
+        Log.e("tag", "dp "+ dp + " dm "+ deviceHeight)
+
+        transYAni(deviceHeight)
     }
-    val testListener = object : Animation.AnimationListener{
-        override fun onAnimationRepeat(p0: Animation?) {
-        }
-
-        override fun onAnimationEnd(p0: Animation?) {
-            imageView.visibility = View.GONE
-        }
-
-        override fun onAnimationStart(p0: Animation?) {
+    private fun transYAni(size : Int) {
+        btn_transY_up.setOnClickListener {
+            imageView.visibility = View.VISIBLE
+            imageView.animate().translationY(-size /3f).scaleX(1.2f).scaleY(1.2f).setDuration(3000L).withEndAction {
+                imageView.scaleX = 0.2f
+                imageView.scaleY = 0.2f
+                imageView.translationY = 0f
+                imageView.visibility = View.GONE
+            }.start()
         }
 
     }
